17f0e59a2217ce12dad11820dde0a044777bbb6b
Travis Kaufman <travis.kaufman@gmail.com>
2012-06-07 00:24:21 -0400

    Set up basic infrastructure

--- MaxCurl.c
+++ MaxCurl.c
@@ -1,56 +1,43 @@
 
-/*
- ===========================================================================================
- Name        : maxcurlleMax.c
- Author      : Cycling '74
- Version     : 5.1.6
- Copyright   : Not mine
- Description : Making the maxcurlleMax Object from C74s Example folder trying to set Xcode up
- ===========================================================================================
+/**
+ * @file MaxCurl.c
+ *
+ * Implementation for MaxCurl. Responsible for instantiating, registering, and 
+ * object-ifying within Max's runtime
  */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <curl/curl.h>
-#include "ext.h"
-#include "ext_obex.h"
+#include "MaxCurl.h"
 
-static t_class *s_maxcurl_class; // global pointer to our class definition that is set up in main()
-
-typedef struct _maxcurl {
-	t_object s_obj; // t_object header
-	long s_value; // something else
-} t_maxcurl;
-
-void* maxcurl_new();
-const char* _maxcurl_doCurl(const char *url);
-void maxcurl_bang(t_maxcurl *x);
-void maxcurl_del();
+// global pointer to our class definition that is set up in main()
+static t_class *s_maxcurl_class; 
+CURL *curl = NULL;
+CURLcode resp = 0;
 
 int main() {
 	t_class *c = NULL;
 	
-	c = class_new("MaxCurl", (method)maxcurl_new, (method)maxcurl_del, sizeof(t_maxcurl), 0L, 0);
-	class_addmethod(c, (method)maxcurl_bang, "bang", 0); // added the method "bang" invoking maxcurl_bang
+	c = class_new("MaxCurl", (method)maxcurl_new, (method)NULL, 
+                sizeof(t_maxcurl), 0L, 0);
+	class_addmethod(c, (method)maxcurl_bang, "bang", 0); 
 	
-	class_register(CLASS_BOX, c); // allows the class to be searched when a user types it into the box
-	
+  // allows the class to be searched when a user types it into the box
+	class_register(CLASS_BOX, c);
 	s_maxcurl_class = c;
-	
 	return EXIT_SUCCESS;
 }
 
 void *maxcurl_new() {
 	t_maxcurl *x = (t_maxcurl *)object_alloc(s_maxcurl_class);
-	x->s_value = 0;
+	x->url = NULL;
 	return x;
 }
 
-const char* _maxcurl_doCurl(const char *url) {
-    // TODO: Implement
-    return "null";
+const char* _maxcurl_doCurl(const char *url, 
+                            size_t (*callback)(char*, size_t, size_t, void*)) {
+  // TODO: Implement
+  return "null";
 }
 
 void maxcurl_bang(t_maxcurl *x) {
-	post("value is %ld", x->s_value);
+	post("Requested url: %s", x->url);
 }
---
+++ MaxCurl.h
@@ -0,0 +1,55 @@
+/**
+ * @file MaxCurl.h
+ * 
+ * Header file for MaxCurl object
+ * 
+ * @author Travis Kaufman
+ */
+#ifndef MaxCurl_MaxCurl_h
+#define MaxCurl_MaxCurl_h
+
+// All includes that we will need
+#include <stdio.h>
+#include <stdlib.h>
+#include <curl/curl.h>
+#include "ext.h"
+#include "ext_obex.h"
+
+/*** MaxCurl Object Interface ***/
+
+// The MaxCurl "Class" Struct
+typedef struct _maxcurl {
+	t_object s_obj; // t_object header
+	char *url; // URL that will be cURL'd
+} t_maxcurl;
+
+/*** Methods (See Implementation file for DocStrings) ***/
+/**
+ * Constructor
+ */
+void* maxcurl_new();
+
+/**
+ * Performs a cURL and returns the data from the page
+ *
+ * @param const char *url
+ *    The url for which you would like to get data from
+ *
+ * @param size_t (*callback)(char*, size_t, size_t, void*)
+ *    The callback function to be performed after the cURL is executed
+ *    Function must subscribe to the following prototype:
+ *      size_t callback(char* ptr, size_t size, size_t nmemb, void *userdata)
+ *    See this link for more info:
+ *    http://curl.haxx.se/libcurl/c/curl_easy_setopt.html#CURLOPTWRITEFUNCTION 
+ */
+const char* _maxcurl_doCurl(const char *url, 
+                            size_t (*callback)(char*, size_t, size_t, void*));
+
+/**
+ * React to "bang" message from MaxMSP client
+ *
+ * @param t_maxcurl *mxcrl
+ *    The MaxCurl object instance 
+ */
+void maxcurl_bang(t_maxcurl *mxcrl);
+#endif
